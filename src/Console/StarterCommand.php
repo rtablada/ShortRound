9faa68393ecc2f84af764adf5d4d865cc54024c1
Ryan Tablada <ryan.tablada@gmail.com>
2015-01-10 13:35:31 -0600

    Start blueprinting

--- src/Console/StarterCommand.php
+++ src/Console/StarterCommand.php
@@ -1,27 +1,31 @@
-<?php namespace Rtablada\ShortRound;
+<?php namespace Rtablada\ShortRound\Console;
 
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
 
-class StarterCommand extends Command {
+class StarterCommand extends Command
+{
 
     /**
      * The console command name.
      *
      * @var string
      */
-    protected $name = 'command:name';
+    protected $name = 'shortround:start';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description.';
+    protected $description = 'Scaffolds new ShortRound CMS.';
+
     /**
      * @var \Illuminate\Filesystem\Filesystem
      */
-    private $file;
+    protected $file;
+
+    protected $starterBlueprintPath;
 
     /**
      * Create a new command instance.
@@ -32,7 +36,14 @@ class StarterCommand extends Command {
     {
         parent::__construct();
         $this->file = $file;
+
+        $this->starterBlueprintPath = __DIR__ . '/../../blueprints/starter';
     }
+
+    protected $blueprintFiles = [
+        'bower.json',
+        'resources/templates/admin',
+    ];
 
     /**
      * Execute the console command.
@@ -42,7 +53,32 @@ class StarterCommand extends Command {
     public function fire()
     {
         $this->file->deleteDirectory(base_path('resources/assets/less'));
-        $this->file->deleteDirectory(app_path('Http/routes.php'));
+        $this->file->deleteDirectory(app_path('Http/Controllers/Auth'));
+        $this->file->delete([app_path('Http/Controllers/HomeController.php'), app_path('Http/Controllers/WelcomController.php')]);
+
+        $this->file->put(app_path('Http/routes.php'), "<?php\n");
+
+        $this->file->append(base_path('.gitignore'), "bower_components\n.vagrant\n.idea\nnode_modules\n");
+
+        foreach ($this->blueprintFiles as $blueprint) {
+            $this->copyBlueprint($blueprint);
+        }
+    }
+
+    protected function copyBlueprint($file)
+    {
+        $blueprintPath = $this->getBlueprint($file);
+
+        if ($this->file->isDirectory($blueprintPath)) {
+            $this->file->copyDirectory($this->getBlueprint($file), base_path($file));
+        } else {
+            $this->file->copy($this->getBlueprint($file), base_path($file));
+        }
+    }
+
+    protected function getBlueprint($file)
+    {
+        return $this->starterBlueprintPath . '/' . $file;
     }
 
     /**
@@ -50,7 +86,8 @@ class StarterCommand extends Command {
      *
      * @return array
      */
-    protected function getArguments()
+    protected
+    function getArguments()
     {
         return [
         ];
@@ -61,7 +98,8 @@ class StarterCommand extends Command {
      *
      * @return array
      */
-    protected function getOptions()
+    protected
+    function getOptions()
     {
         return [
         ];
